f3c10a71ba6022e286b4c6797e008d3cfa411b85
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2022-03-05 05:06:46 +0000

    Test fortran interface in GH actions

---
+++ fortran/runtest.sh
@@ -0,0 +1,2 @@
+python3 -m sparse_ir.dump 1e+4 1e-10 ir_nlambda4_ndigit10.dat
+make test
--- fortran/test.sh
+++
@@ -1,3 +0,0 @@
-#python3 ../src/sparse_ir/dump.py 1e+4 1e-10 ir_nlambda4_ndigit10.dat
-make
-./test_sparse_ir
